ed05c1b581517cec4c4a8073ccd929fe65800730
Daniel Cassidy <mail@danielcassidy.me.uk>
2012-06-21 01:29:02 +0100

    Add net graphic.

--- src/battledore_and_shuttlecock/Net.hx
+++ src/battledore_and_shuttlecock/Net.hx
@@ -1,9 +1,19 @@
 package battledore_and_shuttlecock;
 
+import flash.display.BitmapData;
+import hopscotch.graphics.Image;
 import hopscotch.Entity;
 
 class Net extends Entity {
+    static inline var WIDTH = 8;
+    static inline var HEIGHT = 240;
+
     public function new() {
         super();
+
+        var image = new Image(new BitmapData(WIDTH, HEIGHT, false, 0xffffff));
+        image.originX = WIDTH * 0.5;
+        image.originY = HEIGHT;
+        graphic = image;
     }
 }
