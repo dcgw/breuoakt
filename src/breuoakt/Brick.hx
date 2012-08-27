684d9ed08b801330f849c081653146b82af39074
Daniel Cassidy <mail@danielcassidy.me.uk>
2012-08-27 23:19:03 +0100

    Make bricks pretty colours.

--- src/breuoakt/Brick.hx
+++ src/breuoakt/Brick.hx
@@ -9,20 +9,37 @@ class Brick extends Entity {
     public static inline var WIDTH = 34;
     public static inline var HEIGHT = 14;
 
+    static var colors = [0x99dd92, 0x94c4d3, 0x949ace, 0xcc96b1];
+
+    var images:Array<Image>;
+
     public function new() {
         super();
-        var image = new Image(new BitmapData(WIDTH, HEIGHT, false, 0xffffffff));
-        image.centerOrigin();
-        graphic = image;
 
-        collisionMask = new BoxMask(-image.originX, -image.originY, image.width, image.height);
+        images = [];
+        for (color in colors) {
+            var image = new Image(new BitmapData(WIDTH, HEIGHT, false, color));
+            image.centerOrigin();
+            images.push(image);
+        }
+
+        nextColor();
+
+        collisionMask = new BoxMask(-WIDTH*0.5, -HEIGHT*0.5, WIDTH, HEIGHT);
     }
 
     public function reset() {
-        visible = true;
+        if (!visible) {
+            visible = true;
+            nextColor();
+        }
     }
 
     public function hit() {
         visible = false;
+    }
+
+    function nextColor() {
+        graphic = images[Std.random(images.length)];
     }
 }
