3810cb03a2d45a3efbf0883351b0a10f4063d98b
Daniel Cassidy <mail@danielcassidy.me.uk>
2013-10-29 17:29:39 +0000

    Tweak brick collisions some more.

--- src/breuoakt/BrickCollider.hx
+++ src/breuoakt/BrickCollider.hx
@@ -3,7 +3,10 @@ package breuoakt;
 import hopscotch.Static;
 class BrickCollider {
     private static var MAX_COLLISIONS_PER_BALL = 4;
-    private static var SIZE_TOLERANCE = 4;
+
+    private static var INITIAL_SIZE_TOLERANCE = 2;
+    private static var SIZE_TOLERANCE_INCREMENT = 2;
+    private static var MAX_SIZE_TOLERANCE = Brick.WIDTH;
 
     private var bricks:Array<Brick>;
 
@@ -53,51 +56,57 @@ class BrickCollider {
         brickBelowIndex = -1;
         brickLeftIndex = -1;
 
-        for (i in 0...numCollidedBricks) {
-            brickIndex = collidedBrickIndices[i];
-            var brick = bricks[brickIndex];
+        var sizeTolerance = INITIAL_SIZE_TOLERANCE;
+        while (brickAboveIndex < 0 && brickRightIndex < 0 && brickBelowIndex < 0 && brickLeftIndex < 0
+                && sizeTolerance <= MAX_SIZE_TOLERANCE) {
+            for (i in 0...numCollidedBricks) {
+                brickIndex = collidedBrickIndices[i];
+                var brick = bricks[brickIndex];
 
-            Static.point.x = ball.x;
-            Static.point.y = ball.y;
-            brick.hit(Static.point, ball.velocity);
+                Static.point.x = ball.x;
+                Static.point.y = ball.y;
+                brick.hit(Static.point, ball.velocity);
 
-            if (brick.x + Brick.WIDTH * 0.5 - SIZE_TOLERANCE < ball.x - Ball.WIDTH * 0.5) {
-                brickLeftIndex = brickIndex;
-            } else if (brick.x - Brick.WIDTH * 0.5 + SIZE_TOLERANCE > ball.x + Ball.WIDTH * 0.5) {
-                brickRightIndex = brickIndex;
+                if (brick.x + Brick.WIDTH * 0.5 - sizeTolerance < ball.x - Ball.WIDTH * 0.5) {
+                    brickLeftIndex = brickIndex;
+                } else if (brick.x - Brick.WIDTH * 0.5 + sizeTolerance > ball.x + Ball.WIDTH * 0.5) {
+                    brickRightIndex = brickIndex;
+                }
+
+                if (brick.y + Brick.HEIGHT * 0.5 - sizeTolerance < ball.y - Ball.HEIGHT * 0.5) {
+                    brickAboveIndex = brickIndex;
+                } else if (brick.y - Brick.HEIGHT * 0.5 + sizeTolerance > ball.y + Ball.HEIGHT * 0.5) {
+                    brickBelowIndex = brickIndex;
+                }
             }
 
-            if (brick.y + Brick.HEIGHT * 0.5 - SIZE_TOLERANCE < ball.y - Ball.HEIGHT * 0.5) {
-                brickAboveIndex = brickIndex;
-            } else if (brick.y - Brick.HEIGHT * 0.5 + SIZE_TOLERANCE > ball.y + Ball.HEIGHT * 0.5) {
-                brickBelowIndex = brickIndex;
-            }
-        }
-
-        if ((brickRightIndex >= 0 && brickLeftIndex < 0 && ball.velocity.x > 0)
-                || (brickLeftIndex >= 0 && brickRightIndex < 0 && ball.velocity.x < 0)) {
-            ball.velocity.x = -ball.velocity.x;
-        }
-
-        if ((brickBelowIndex >= 0 && brickAboveIndex < 0 && ball.velocity.y > 0)
-                || (brickAboveIndex >= 0 && brickBelowIndex < 0 && ball.velocity.y < 0)) {
-            ball.velocity.y = -ball.velocity.y;
-        }
-
-        if (brickAboveIndex < 0 && brickRightIndex < 0 && brickBelowIndex < 0 && brickLeftIndex < 0) {
-            ball.velocity.x = -ball.velocity.x;
-            ball.velocity.y = -ball.velocity.y;
+            sizeTolerance += SIZE_TOLERANCE_INCREMENT;
         }
 
         var hitTop = false;
 
-        if (brickBelowIndex >= 0 && brickAboveIndex < 0) {
-            brickIndex = brickBelowIndex;
-            while (brickIndex >= 0 && !bricks[brickIndex].collidable) {
-                brickIndex -= Game.NUM_BRICKS_X;
+        if (sizeTolerance > MAX_SIZE_TOLERANCE) {
+            ball.velocity.x = -ball.velocity.x;
+            ball.velocity.y = -ball.velocity.y;
+        } else {
+            if ((brickRightIndex >= 0 && brickLeftIndex < 0 && ball.velocity.x > 0)
+                    || (brickLeftIndex >= 0 && brickRightIndex < 0 && ball.velocity.x < 0)) {
+                ball.velocity.x = -ball.velocity.x;
             }
 
-            hitTop = brickIndex < 0;
+            if ((brickBelowIndex >= 0 && brickAboveIndex < 0 && ball.velocity.y > 0)
+                    || (brickAboveIndex >= 0 && brickBelowIndex < 0 && ball.velocity.y < 0)) {
+                ball.velocity.y = -ball.velocity.y;
+            }
+
+            if (brickBelowIndex >= 0 && brickAboveIndex < 0) {
+                brickIndex = brickBelowIndex;
+                while (brickIndex >= 0 && !bricks[brickIndex].collidable) {
+                    brickIndex -= Game.NUM_BRICKS_X;
+                }
+
+                hitTop = brickIndex < 0;
+            }
         }
 
         onHit(ball, numCollidedBricks, hitTop);
